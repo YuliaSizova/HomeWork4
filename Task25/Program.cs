7a7da37dbdd7afedb79d6b47307b54bd572106e1
ilia <ilia100@hotmail.com>
2023-01-29 22:50:02 +0300

    Выполнена 4 домашняя работа

---
+++ Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("Введите число A ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+static int Exponentiation(int num1, int num2)
+
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = result * num1;
+    }
+
+    return result;
+}
+int pow = Exponentiation(numA, numB);
+
+System.Console.WriteLine(pow);
---
+++ Task27/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+static int GetCountNum(int numx)
+{
+
+    int count = 0;
+
+    while (numx > 0)
+    {
+        count++;
+        numx /= 10;
+    }
+    return count;
+}
+
+int countrs = GetCountNum(num);
+
+static int GetSumCount(int count, int number)
+{
+    int result = 0;
+    int temp = number;
+    for (int i = 0; i <= count; i++) 
+    {
+        result = result + temp % 10;
+        temp = temp / 10;
+    }
+    return result;
+}
+int sum = GetSumCount(countrs, num);
+Console.WriteLine(sum);
